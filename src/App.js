77cb4ea04767c6c4872d55e38b51c43df41a4290
Vlad Seheda <vseheda@s-pro.io>
2021-12-17 02:22:35 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactMapGL from 'react-map-gl';
 
 function App() {
+  const [viewport, setViewport] = React.useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container ">
+    <div className="phone">
+      <div className="power" />
+      <div className="photo" />
+      <div className="voiceP" />
+      <div className="voiceM" />
+      <div className="map">
+        <div className="map-container" style={{ height: 'inherit', width: '100%', borderRadius: '15px' }}>
+          <ReactMapGL
+            {...viewport}
+            width="00px"
+            height="500px"
+            onViewportChange={(viewport) => setViewport(viewport)}
+            mapboxApiAccessToken="pk.eyJ1IjoidmliZXNwb3QiLCJhIjoiY2p4YnN5cHY3MDB3NDN4czVrOWtpanU4aCJ9.nexbisROPDgIPDPUPm5tvQ"
+          />
+        </div>
+      </div>
+    </div>
+      </div>
     </div>
   );
 }
